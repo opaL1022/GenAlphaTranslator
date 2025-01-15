8f513064dbb644cbdf3b3244106cba9489a93457
opaL1022 <whitestoneabc123@gmail.com>
2025-01-15 16:15:23 +0800

    補齊檔案
    修改開關的commit放錯檔案 現在補上

--- public/popup.js
+++ public/popup.js
@@ -4,8 +4,20 @@
   !*** ./src/popup.ts ***!
   \**********************/
 
-document.getElementById('actionButton')?.addEventListener('click', () => {
-    alert('Button clicked!');
+const circleToggle = document.getElementById('circleToggle');
+const circleSwitch = document.getElementById('circleSwitch');
+if (!circleToggle || !circleSwitch) {
+    throw new Error('Elements not found');
+}
+circleToggle.addEventListener('change', () => {
+    if (circleToggle.checked) {
+        circleSwitch.style.backgroundColor = '#4CAF50'; // ON 状态背景色
+        console.log('Switch is ON');
+    }
+    else {
+        circleSwitch.style.backgroundColor = '#ccc'; // OFF 状态背景色
+        console.log('Switch is OFF');
+    }
 });
 
 /******/ })()
