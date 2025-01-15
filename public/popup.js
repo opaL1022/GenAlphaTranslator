e09bec905692fec2074710053deb0f9831b14a0d
Max7414 <90671891+Max7414@users.noreply.github.com>
2025-01-15 21:49:00 +0800

    title color changed

--- public/popup.js
+++ public/popup.js
@@ -1,1 +1,32 @@
+<<<<<<< Updated upstream
 (()=>{"use strict";const e=document.getElementById("circleToggle"),c=document.getElementById("circleSwitch");if(!e||!c)throw new Error("Elements not found");e.addEventListener("change",(()=>{e.checked?(c.style.backgroundColor="#4CAF50",console.log("Switch is ON")):(c.style.backgroundColor="#ccc",console.log("Switch is OFF"))}))})();
+=======
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./src/popup.ts ***!
+  \**********************/
+
+const circleToggle = document.getElementById('circleToggle');
+const circleSwitch = document.getElementById('circleSwitch');
+const changeTitle = document.getElementById('title');
+if (!circleToggle || !circleSwitch || !changeTitle) {
+    throw new Error('Elements not found');
+}
+circleToggle.addEventListener('change', () => {
+    if (circleToggle.checked) {
+        circleSwitch.style.backgroundColor = '#4CAF50'; // ON 状态背景色
+        console.log('Switch is ON');
+        changeTitle.style.color = '#4CAF50';
+    }
+    else {
+        circleSwitch.style.backgroundColor = '#ccc'; // OFF 状态背景色
+        console.log('Switch is OFF');
+        changeTitle.style.color = '#ccc';
+    }
+});
+
+/******/ })()
+;
+//# sourceMappingURL=popup.js.map
+>>>>>>> Stashed changes
