c3d192aff5dc49074f185e46cca79bdbba3203cb
opaL1022 <whitestoneabc123@gmail.com>
2025-01-15 15:01:52 +0800

    開關時游標變化與配置watch模式
    游標放上開關時會變成pointer
    配置文件已啟用watch模式

---
+++ public/content.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!************************!*\
+  !*** ./src/content.ts ***!
+  \************************/
+
+console.log('Content script loaded!');
+
+/******/ })()
+;
+//# sourceMappingURL=content.js.map
---
+++ public/popup.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./src/popup.ts ***!
+  \**********************/
+
+document.getElementById('actionButton')?.addEventListener('click', () => {
+    alert('Button clicked!');
+});
+
+/******/ })()
+;
+//# sourceMappingURL=popup.js.map
